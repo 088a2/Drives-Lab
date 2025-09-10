cb660e8be0e01c5de9f68ce901c34b061bf0465c
Mrinal <158087568+088a2@users.noreply.github.com>
2025-09-10 18:08:20 +0530

    Lab 6 initial update

---
+++ Lab 6/Lab_6.m
@@ -0,0 +1,45 @@
+% First
+s = tf("s");
+G = 1 / (s^2 + 10 * s + 20);
+
+T = feedback(G, 1);
+step(T)
+
+% Second
+s = tf("s");
+G = 1 / (s^2 + 10 * s + 20);
+H = 1;
+
+T = feedback(G, H);
+step(T, [0:.01:2])
+
+% Third
+s = tf("s");
+G = 1 / (s^2 + 10 * s + 20);
+kp = 50;
+C = pid(kp);
+
+T = feedback(C * G, 1);
+step(T, [0:.01:2])
+
+% Fourth
+s = tf("s");
+G = 1 / (s^2 + 10 * s + 20);
+kp = 300;
+C = pid(kp);
+
+T = feedback(C * G, 1);
+step(T, [0:.01:2])
+
+hold on
+
+% Fifth
+s = tf("s");
+G = 1 / (s^2 + 10 * s + 20);
+kp = 300; ki = 300; kd = 10;
+
+C = pid(kp, ki, kd);
+T = feedback(C * G, 1);
+step(T, [0:.01:2])
+
+hold on
