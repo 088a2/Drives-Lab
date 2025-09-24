4a7cabf2e22caee3b56e508c4777703b38250bff
Mrinal <158087568+088a2@users.noreply.github.com>
2025-09-24 15:50:09 +0530

    Lab 7 Initial Update

--- Lab 6/Lab_6.m
+++ Lab 6/Lab_6.m
@@ -42,4 +42,11 @@ C = pid(kp, ki, kd);
 T = feedback(C * G, 1);
 step(T, [0:.01:2])
 
+s=tf("s");
+G = 1/(s^2 +10*s+20);
+kp=300; ki=300;
+C=pid(kp,ki);
+T = feedback(C*G,1);
+step(T,[0:.01:2])
+
 hold on
